d243ad2d029ca6472aa273f3b2d319c458fe73bf
ananthuuu <ananthcjayan12@gmail.com>
2025-04-23 18:00:14 +0530

    removed unique constraints for train

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Train(models.Model):
-    train_number = models.CharField(max_length=10, unique=True)
+    train_number = models.CharField(max_length=10)
     station = models.CharField(max_length=100)
     time = models.TimeField()
     week_day = models.CharField(max_length=50, help_text="Comma-separated days or 'All'")
